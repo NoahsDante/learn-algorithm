93c707c7b064296b045c27d2d58a9339931299c7
散开一群逗比 散一群逗比 <15257614673@163.com>
2022-01-06 01:08:14 +0800

    首次提交 选择排序、冒泡排序

---
+++ js/bubbleSort.js
@@ -0,0 +1,21 @@
+/**
+ * 选择排序
+ * @param arr
+ * @returns {*}
+ */
+function bubbleSort(arr) {
+  if (arr == null || arr.length < 2) {
+    return;
+  }
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if(arr[j] > arr[j+1]) { // 找到最大排序
+        swap(arr,j,j+1)
+      }
+    }
+  }
+  return arr;
+}
+const test1 = generateRandomArray(10);
+jsLog('原始',test1)
+jsLog('转换',bubbleSort(test1))
